5ff9902c6319e2c46c49bb7ce3752d374bc03d68
Kyle Kopicki <kyle.kopicki@gmail.com>
2017-02-23 00:51:04 -0500

    patient testimonials has star wars patient testimonials now.

--- app/scripts/controllers/difference/testimonialsController.js
+++ app/scripts/controllers/difference/testimonialsController.js
@@ -1,8 +1,16 @@
 (function() {
     'use strict';
-    function TestimonialsController($log) {
+    function TestimonialsController($log, $scope, $interval) {
         var vm = this;
         $log.debug('Testimonials Controller');
+        $interval(function(){
+            var pt = angular.element('.wrapper')
+                .addClass('hvr-glimmer');
+        }, 6000);
+        angular.element('.console button').on('click', function () {
+            angular.element('.wrapper').removeClass('hvr-glimmer');
+        });
+
     }
 
     angular.module('gtoApp').controller('testimonialsCtrl', TestimonialsController);
